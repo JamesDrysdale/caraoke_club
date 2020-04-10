61c23d682af159eec25244b4e10ca57d35d53676
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-10 18:22:45 +0100

    Set up test for room ID to check a room can have an ID

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,6 +7,16 @@ require_relative( '../room' )
 
 class TestRoom < MiniTest::Test
 
+    # A room has an ID(room number), capacity and occupancy
+
+    def setup
+        @room1 = Room.new("1", 12, 20)
+        @room2 = Room.new("2", 20, 20)
+    end
+
+    def test_room_id()
+        assert_equal("1", @room1.name)
+    end
 
 
 end
