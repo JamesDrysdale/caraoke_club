fc85c9a77aecd60d25090b5b2d0316592169b53f
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-10 18:10:43 +0100

    Set up sekeleton page for guest  spec

---
+++ specs/guest_spec.rb
@@ -0,0 +1,12 @@
+require( 'minitest/autorun' )
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative( '../guest' )
+
+
+class TestGuest < MiniTest::Test
+
+
+
+end
